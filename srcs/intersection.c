d9c0df86b39514ec7f97ac1a40f2c59b03c39d3e
Florian DE SOUZA <fde-souz@student.42.fr>
2018-02-05 13:12:52 +0100

    intersection

--- srcs/intersection.c
+++ srcs/intersection.c
@@ -6,7 +6,7 @@
 /*   By: fde-souz <fde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 13:10:08 by fde-souz          #+#    #+#             */
-/*   Updated: 2018/02/05 11:41:05 by fde-souz         ###   ########.fr       */
+/*   Updated: 2018/02/05 13:11:26 by fde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,12 @@ t_intersection find_intersection_hor(double alpha, t_win_info w)
 	}
 	a.x = w.player.pos_x + (w.player.pos_y - a.y) / tan(alpha * RAD);
 	xa *= BLOC / tan(alpha * RAD);
-	if (!((int)(a.x / BLOC) > 32 || (int)(a.x / BLOC) < 0 || (int)(a.y / BLOC) > 32 || (int)(a.y / BLOC) < 0))
+	while (((int)(a.x / BLOC) < 32 && (int)(a.x / BLOC) > 0) && (int)(a.y / BLOC) < 32 && (int)(a.y / BLOC) > 0)
 	{
-		while (w.map[(int)(a.y / BLOC)][(int)(a.x / BLOC)] != WALL)
-		{
-			if ((int)(a.x / BLOC) > 32 || (int)(a.x / BLOC) < 0 || (int)(a.y / BLOC) > 32 || (int)(a.y / BLOC) < 0)
-				return (a);
-			a.x += xa;
-			a.y += ya;
-		}
+		if (w.map[(int)(a.y / BLOC)][(int)(a.x / BLOC)] == WALL)
+			return (a);
+		a.x += xa;
+		a.y += ya;
 	}
 	return (a);
 }
@@ -51,7 +48,7 @@ t_intersection find_intersection_ver(double alpha, t_win_info w)
 	double			xa;
 	double			ya;
 
- 	if (cos(alpha * RAD) > 0)
+	if (cos(alpha * RAD) > 0)
 	{
 		a.x = (int)(w.player.pos_x / BLOC) * BLOC + BLOC;
 		xa = BLOC;
@@ -65,15 +62,12 @@ t_intersection find_intersection_ver(double alpha, t_win_info w)
 	}
 	a.y = w.player.pos_y + (w.player.pos_x - a.x) * tan(alpha * RAD);
 	ya *= BLOC * tan(alpha * RAD);
-	if (!((int)(a.x / BLOC) > 32 || (int)(a.x / BLOC) < 0 || (int)(a.y / BLOC) > 32 || (int)(a.y / BLOC) < 0))
+	while (((int)(a.x / BLOC) < 32 && (int)(a.x / BLOC) > 0) && (int)(a.y / BLOC) < 32 && (int)(a.y / BLOC) > 0)
 	{
-		while (w.map[(int)(a.y / BLOC)][(int)(a.x / BLOC)] != WALL)
-		{
-			if ((int)(a.x / BLOC) > 32 || (int)(a.x / BLOC) < 0 || (int)(a.y / BLOC) > 32 || (int)(a.y / BLOC) < 0)
-				return (a);
-			a.x += xa;
-			a.y += ya;
-		}
+		if (w.map[(int)(a.y / BLOC)][(int)(a.x / BLOC)] == WALL)
+			return (a);
+		a.x += xa;
+		a.y += ya;
 	}
 	return (a);
 }
@@ -83,7 +77,6 @@ void	draw(int x, int h_wall, t_win_info *w)
 	int y;
 
 	y = SIZE_Y / 2 - h_wall / 2;
-//	printf("%d\n", h_wall);
 	while(y < SIZE_Y / 2 + h_wall / 2)
 	{
 		put_pixel_image(x, y, 0xff0000, w);
@@ -112,12 +105,12 @@ int		raycasting(t_win_info w)
 	while (x <= SIZE_X)
 	{
 		alpha = (w.player.dir_x + (w.player.fov / 2)) - ((w.player.fov / SIZE_X) * x);
-		a.x = -1;
-		a.y = -1;
-		b.x = -1;
-		b.y = -1;
-		pa = -1;
-		pb = -1;
+		a.x = 10000;
+		a.y = 10000;
+		b.x = 10000;
+		b.y = 10000;
+		pa = 10000;
+		pb = 10000;
 		if (sin(alpha * RAD))
 			a = find_intersection_hor(alpha, w);
 		if (cos(alpha * RAD))
@@ -126,7 +119,6 @@ int		raycasting(t_win_info w)
 		pb = sqrt(powf((w.player.pos_x - b.x), 2) + powf((w.player.pos_y - b.y), 2));
 		beta = alpha > w.player.dir_x ? 30 : -30;
 		dist = pa > pb ? pb * cos(beta * RAD) : pa * cos(beta * RAD);
-	//	printf("%d %d\n", h_wall, dist);
 		h_wall = dist < 10 ? SIZE_Y : BLOC / dist * w.dist_player_proj;
 		draw(x, h_wall, &w);
 		x++;
